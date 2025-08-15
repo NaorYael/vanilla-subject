7ca34293d0a9d6ec3199f1807f99b63d9e2a0b4a
Naor Yael <47955339+NaorYael@users.noreply.github.com>
2025-08-15 11:59:06 +0300

    feat: add recursive flat() example

---
+++ flat.js
@@ -0,0 +1,15 @@
+﻿const arr = [1,4,9,[3,2,[1,7]], 4];
+let temp = [];
+
+function flat(numArr) {
+  numArr.forEach(item => {
+    if (Array.isArray(item)) {
+      flat(item);
+    } else {
+      temp.push(item);
+    }
+  });
+  return temp;
+}
+
+console.log(flat(arr));
